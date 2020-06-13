7d1212a02d53721c422d45b004cebf3a2b55069f
陈叶枫 <1449148932@qq.com>
2020-06-13 23:15:04 +0800

    第一次提交

---
+++ js/app.js
@@ -0,0 +1,101 @@
+(function (Vue) {
+	const STORAGE_KEY = 'item';
+	const localstorage ={
+		fetch:function(){
+
+			return JSON.parse(
+				localStorage.getItem(STORAGE_KEY)||'[]'
+			)
+		},
+		save:function(items){
+
+			localStorage.setItem(STORAGE_KEY,JSON.stringify(items))
+		}
+	}
+
+	const items=[
+
+	]
+	var vm = new Vue({
+		el:'#todoapp',
+		data:{
+			title: 'cyf',
+			items:localstorage.fetch(),
+			status:'all',
+			currentItem:null,
+		},
+
+		computed:{
+			filter() {
+				switch(this.status){
+					case 'active':
+						return this.items.filter(function(i){
+							return i.completed === false
+						})
+						break;
+					case 'completed':
+						return this.items.filter(function(i){
+							return i.completed === true
+						});
+						break;
+					default:
+						return this.items;
+						break;
+				}
+			},
+
+			remain() {
+				const unitems = this.items.filter(function(i){
+					return i.completed === false;
+				});
+				return unitems.length;
+			}
+
+		},
+		methods:{
+			add(e) {
+				const content = e.target.value.trim();
+				if(!content.length){
+					return;
+				}
+				else{
+					const id = this.items.length +1;
+					this.items.push(
+						{
+							id:id,
+							content:content,
+							completed:false,
+						}
+					)
+				}
+				e.target.value = ''
+
+			},
+			remove(index) {
+				this.items.splice(index,1);
+
+			},
+			clearItems() {
+				this.items = this.items.filter(function(i){
+					return i.completed === false;
+				})
+			},
+
+		},
+		watch: {
+			items:{
+				deep:true,
+				handler:function(newItems,oldItems){
+					localstorage.save(newItems)
+				}
+			}
+		},
+
+	})
+	window.onhashchange = function(){
+		const hash = window.location.hash.substr(2)||'all';
+		vm.filterStatus = hash;
+	}
+	window.onhashchange();
+
+})(Vue);
